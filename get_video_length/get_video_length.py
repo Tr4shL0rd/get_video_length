80a0b4ec6aa23a0f245f0f8994386858a911bd89
Tr4shL0rd <ketil04@gmail.com>
2022-09-30 14:57:11 +0200

    added id validation and made it possible to get data from a URL

--- get_video_length/get_video_length.py
+++ get_video_length/get_video_length.py
@@ -1,20 +1,120 @@
 """Main module."""
 import cv2
+import os.path
+import os
+import youtube_dl
+import requests
+from PIL import Image
+from io import BytesIO
+
 class gvl:
     """
         Help on package gvl\n
+
         NAME
             gvl
+
         DESCRIPTION
             gvl (get video length) is a package for fetching the length of videos
     """
-    
-    def video_file(filename:str) -> tuple[float, int, float]:
+
+    def __init__(self) -> None:
+        pass
+    @classmethod
+    def _get_image_dims(self,url:str):
+        data = requests.get(url).content
+        im = Image.open(BytesIO(data))    
+        return im.size
+    @classmethod
+    def _check_offline_video_id_validity(self,url:str) -> bool:
         """
+            Checks the video id length
+            note, this does not check if the video actually exists
+        """
+        # checks if the given url is valid and if the video id is 11 characters long
+        return url.startswith("https://") and len(url.rsplit("v=")[1]) == 11 or len(url.rsplit("/")) == 11
+    
+    @classmethod
+    def _get_url_id(self,url:str) -> str:
+        if url.startswith("https://"):
+            return url.rsplit("v=")[1]
+        return url.rsplit("/")
+    @classmethod
+    def _check_online_video_id_validity(self,url:str) -> bool:
+        """
+            check if the video exists on youtube
+        """
+        checker_url = f"https://img.youtube.com/vi/{self._get_url_id(url)}/mqdefault.jpg"
+        r = requests.get(checker_url)
+        invalid_image_dimensions = (120,90)
+        #print(self._get_image_dims(checker_url))
+        return True if self._get_image_dims(checker_url) != invalid_image_dimensions else False
+        
+        
+
+    @classmethod
+    def video_file(self,filename:str) -> tuple[float, int, float]:
+        """
+        gvl.video_file() returns the duration, the frame count and the fps of the givin video file
+
             PARAMETERS
             ----------
                 filename : str
-                    the location of the video file
+                    The location of the video file
+
+            USAGE
+            -----
+                >>> gvl.video_file("videos/superAwsomeSong.mp4")
+                (9.8, 294, 30.0)
+
+            RETURN
+            ------
+                duration : float
+                    The video length in seconds
+                fram_count : int
+                    The amount of frames in the video
+                fps : float
+                    The videos FPS
+
+            RAISE
+            -----
+                FileNotFoundError : Exception
+                    File could not be found
+                ZeroDivisionError : Exception
+                    File might not be an actual video file
+        """
+        # Checks if the file exists 
+        if not os.path.isfile(filename):
+            raise FileNotFoundError(f"File \"{filename}\" could not be found!")
+        
+        # Gets the video data
+        video = cv2.VideoCapture(filename)
+        # Gets the FPS the video is running at
+        fps         = video.get(cv2.CAP_PROP_FPS)
+        # Gets the amount of frames the video have
+        frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
+        # Checks if either fps or frame_count is 0 or less
+        if fps <= 0 or frame_count <= 0:
+            raise ZeroDivisionError("either FPS or FRAME_COUNT is 0, are you sure it's a video file?")
+        # Calculates the duration by dividing frame_count by fps
+        duration    = frame_count/fps
+        return (duration, int(frame_count),fps)
+
+    @classmethod
+    def video_url(self,url:str) -> tuple[float,int,float]:
+        """
+        gvl.video_url() returns the duration, the frame count and the fps of the givin video
+        please note that gvl.video_url() is less precise than gvl.video_file()
+
+            PARAMETERS
+            ----------
+                url : str
+                    the url of the video
+
+            USAGE
+            -----
+                >>> gvl.video_url("www.example.com/video.mp4")
+                (9.8, 294, 30.0)
 
             RETURN
             ------
@@ -25,18 +125,21 @@ class gvl:
                 fps : float
                     the videos FPS
 
-            USAGE
+            RAISE
             -----
-                >>> gvl.video_file("videos/superAwsomeSong.mp4")
-                (9.8, 294, 30.0)
+                FileNotFoundError : Exception
+                    file could not be found
         """
-        video = cv2.VideoCapture(filename)
-
-        fps         = video.get(cv2.CAP_PROP_FPS)
-        frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
-        duration    = frame_count/fps
-
-        return (duration, int(frame_count),fps)
-    
-    def video_url(url:str) -> tuple[float,int,float]:
-        ...
+        ytdl_opts = {
+                    "forceurl":True,
+                    "quiet": True,
+                }
+        with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
+            r = ytdl.extract_info(url, download=False)
+            
+            #raise youtube_dl.utils.DownloadError(f"Could not download \"{url}\"")
+        fps = r["fps"]
+        duration = r["duration"]
+        # Calculates the frame count by multiplying the videos frames per second with the duration
+        frame_count = int(fps) * float(duration)
+        return (duration, int(frame_count), fps)
